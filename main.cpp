8168f8c2cde0bbf01783cfcb8fbdc61e902425ab
Nathan Greco <nathan.greco@gmail.com>
2017-03-01 01:04:58 +0000

    Finalized solution

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@
 
 int main()
 {
-	const int kpixwidth{ 768 * 2 };		//Must be multiple of 128!
+	const int kpixwidth{ 800 * 2 };
 	const int kpixheight{ 480 };
 	std::cout << "Program launched, capture image resolution will be " << kpixwidth <<
 				 "x" << kpixheight << '\n';
@@ -67,7 +67,7 @@ int main()
 	camera0.set( CV_CAP_PROP_FRAME_HEIGHT, kpixheight );
 	camera0.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
 	//Set stereoscopic mode
-	if ( !camera0.setStereoMode(1, false) ) {
+	if ( !camera0.setStereoMode(1) ) {
 		std::cerr << "Error setting stereoscopic mode" << '\n';
 		exit(-1);
 	}
@@ -78,39 +78,21 @@ int main()
 		std::cerr << "Error opening the camera 0" << '\n';
 		exit(-1);
 	}
-/*
-	//Create camera 1
-	std::cout << "Creating camera 1..." << '\n';
-	raspicam::RaspiCam_Cv camera1;
-	//Set properties
-	camera1.set( CV_CAP_PROP_FRAME_WIDTH, kpixwidth );
-	camera1.set( CV_CAP_PROP_FRAME_HEIGHT, kpixheight );
-	camera1.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
-	//Create window
-	cv::namedWindow( "Output1", cv::WINDOW_NORMAL );
-	//Validate open
-	if ( !camera1.open( 1 ) ) {
-		std::cerr << "Error opening the camera 1" << '\n';
-		exit(-1);
-	}
-*/
+
 	//Create disparity window
-	//cv::namedWindow( "Output2", cv::WINDOW_NORMAL );
+	//cv::namedWindow( "Output1", cv::WINDOW_NORMAL );
 
 	//Create pace setter to maintain FPS
 	PaceSetter camerapacer( 5, "Main thread" );
 	
 	//Loop indefinitely
 	for ( ;; ) {
-		//Grab images - Will need to make simultaneous threads in future synchronization
+		//Grab stereo image
 		camera0.grab();
-		//camera1.grab();
 
 		//Copy to cv::Mat
 		cv::Mat image0;
-		//cv::Mat image1;
 		camera0.retrieve( image0 );
-		//camera1.retrieve( image1 );
 		
 		//Create disparity map
 		//v::StereoBM* stereobm = cv::createStereoBM();
@@ -119,8 +101,7 @@ int main()
 		
 		//Update windows
 		cv::imshow( "Output0", image0 );
-		//cv::imshow( "Output1", image1 );
-		//cv::imshow( "Output2", disparity );
+		//cv::imshow( "Output1", disparity );
 		cv::waitKey( 1 );
 	
 		//Set pace
